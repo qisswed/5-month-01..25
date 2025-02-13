3901ff70ab5af298d9765e08756f70e8e4496840
Beknazar Baktybekov <baktybekovbeknazar4@gmail.com>
2025-02-13 13:53:39 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import CounterPage from './pages/CounterPage'
+
+const App = () => (
+    <Provider store={store}>
+        <CounterPage/>
+    </Provider>
+);
+
+export default App;
---
+++ store.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+import CounterReducer from './reducer';
+import CounterPage from "../pages/CounterPage";
+const store = createStore(CounterReducer);
+export default store;
